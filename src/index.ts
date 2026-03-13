8c7f2a3c72638327346a120df90f4e27b7663c2b
victoriacarvalho <victoriacostasilvac@gmail.com>
2026-03-13 15:22:40 -0300

    chore: add better auth setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 
+import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import Fastify from "fastify";
@@ -10,6 +11,8 @@ import {
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import z from "zod";
+
+import auth from "./lib/auth.js";
 
 const app = Fastify({
   logger: true,
@@ -39,6 +42,11 @@ await app.register(fastifySwaggerUI, {
   routePrefix: "/docs",
 });
 
+await app.register(fastifyCors, {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+});
+
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
   url: "/",
@@ -53,6 +61,39 @@ app.withTypeProvider<ZodTypeProvider>().route({
   },
   handler: (req, res) => {
     res.send({ message: "Hello World" });
+  },
+});
+
+app.route({
+  method: ["GET", "POST"],
+  url: "/api/auth/*",
+  async handler(request, reply) {
+    try {
+      const url = new URL(request.url, `http://${request.headers.host}`);
+
+      const headers = new Headers();
+      Object.entries(request.headers).forEach(([key, value]) => {
+        if (value) headers.append(key, value.toString());
+      });
+
+      const req = new Request(url.toString(), {
+        method: request.method,
+        headers,
+        ...(request.body ? { body: JSON.stringify(request.body) } : {}),
+      });
+
+      const response = await auth.handler(req);
+
+      reply.status(response.status);
+      response.headers.forEach((value, key) => reply.header(key, value));
+      reply.send(response.body ? await response.text() : null);
+    } catch (error) {
+      app.log.error(error);
+      reply.status(500).send({
+        error: "Internal authentication error",
+        code: "AUTH_FAILURE",
+      });
+    }
   },
 });
 
